58dfa340ecfad2ca5f17ffa3b7953a421c8a7a61
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-09 01:16:40 +0530

    finishing

---
+++ index.js
@@ -0,0 +1,27 @@
+// Sample data for autocomplete suggestions
+const suggestions = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grapes'];
+
+const input = document.getElementById("autocomplete-input");
+const list = document.getElementById("autocomplete-list");
+
+function updateAutoComplete(){
+    const searchTerm = input.value.toLowerCase();
+    const filteredSuggestions = suggestions.filter(suggestion=>suggestion.toLowerCase().startsWith(searchTerm));
+
+    //clear the prev suggestion
+     list.innerHTML="";
+
+     //render filtered suggestions in the UI
+    filteredSuggestions.forEach(suggestion=>{
+        const li = document.createElement('li');
+        li.textContent = suggestion;
+        li.addEventListener('click',()=>{
+            input.value = suggestion;
+            list.innerHTML="";
+        })
+        list.appendChild(li)
+    })
+    
+}
+
+input.addEventListener("input",updateAutoComplete);
