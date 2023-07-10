36cc4815b08d8a9fe653252ccc50fbc9e5505a07
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-10 23:46:04 +0530

    minor

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const suggestions = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', '
 const input = document.getElementById("autocomplete-input");
 const list = document.getElementById("autocomplete-list");
 
-function updateAutoComplete(){
+function updateAutoComplete() {
     const searchTerm = input.value.toLowerCase();
-    const filteredSuggestions = suggestions.filter(suggestion=>suggestion.toLowerCase().startsWith(searchTerm));
+    const filteredSuggestions = suggestions.filter(suggestion => suggestion.toLowerCase().startsWith(searchTerm));
 
     //clear the prev suggestion
-     list.innerHTML="";
+    list.innerHTML = "";
 
-     //render filtered suggestions in the UI
-    filteredSuggestions.forEach(suggestion=>{
+    //render filtered suggestions in the UI
+    filteredSuggestions.forEach(suggestion => {
         const li = document.createElement('li');
         li.textContent = suggestion;
-        li.addEventListener('click',()=>{
+        li.addEventListener('click', () => {
             input.value = suggestion;
-            list.innerHTML="";
+            list.innerHTML = "";
         })
         list.appendChild(li)
     })
-    
+
 }
 
-input.addEventListener("input",updateAutoComplete);
+input.addEventListener("input", updateAutoComplete);
